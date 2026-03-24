e59afa5fed9c4df1fe50d1cd0aa81849718b8c65
Monika Fil <monia8805@gmail.com>
2026-03-24 12:37:11 +0100

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,82 @@
+from sqlalchemy import Table, Column, Integer, String, Float, Date, MetaData
+from sqlalchemy import create_engine
+import csv
+from datetime import datetime
+
+# Połączenie
+engine = create_engine("sqlite:///weather.db")
+meta = MetaData()
+
+stations = Table(
+    "stations", meta,
+    Column("station", String, primary_key=True),
+    Column("latitude", Float),
+    Column("longitude", Float),
+    Column("elevation", Float),
+    Column("name", String),
+    Column("country", String),
+    Column("state", String),
+)
+
+measurements = Table(
+    "measurements", meta,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("station", String),
+    Column("date", Date),
+    Column("precip", Float),
+    Column("tobs", Float),
+)
+
+# Tworzenie tabel
+meta.create_all(engine)
+
+print("Tabele w bazie:", engine.table_names())
+
+# Połączenie
+conn = engine.connect()
+
+# -----------------------------
+# Wczytywanie clean_stations.csv
+# -----------------------------
+with open("clean_stations.csv") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        conn.execute(
+            stations.insert().values(
+                station=row["station"],
+                latitude=float(row["latitude"]),
+                longitude=float(row["longitude"]),
+                elevation=float(row["elevation"]),
+                name=row["name"],
+                country=row["country"],
+                state=row["state"]
+            )
+        )
+
+# -----------------------------
+# Wczytywanie clean_measure.csv
+# -----------------------------
+with open("clean_measure.csv") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        date = datetime.strptime(row["date"], "%Y-%m-%d").date()
+        conn.execute(
+            measurements.insert().values(
+                station=row["station"],
+                date=date,
+                precip=float(row["precip"]),
+                tobs=float(row["tobs"])
+            )
+        )
+
+# Test
+print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+
+
+
+
+
+
+
+
+
